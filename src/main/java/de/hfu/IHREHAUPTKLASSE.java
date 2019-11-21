6948afe21b3cdc3158d4dc666ab67d5f50739705
SirTunahead <57484836+SirTunahead@users.noreply.github.com>
2019-11-21 12:53:47 +0100

    Added Files via Upload

---
+++ src/main/java/de/hfu/IHREHAUPTKLASSE.java
@@ -0,0 +1,16 @@
+package de.hfu;
+
+/**
+ * Hello world!
+ *
+ */
+public class IHREHAUPTKLASSE 
+{
+    public static void main( String[] args )
+    {
+	    String eingabe = "Hallo Welt";
+	    String eingabeUpper = eingabe.toUpperCase();
+
+	    System.out.println(eingabeUpper);
+    }
+}
